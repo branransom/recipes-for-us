ae41a0e31af9776320271942b76c23d0f876b6a8
Brandon Ransom <branransom@gmail.com>
2020-01-05 17:58:44 -0500

    Add auto-reload capability for UI

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,39 +3,41 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 class RecipeList extends React.Component {
-  state = {
-    recipes: []
-  };
+    state = {
+        recipes: [],
+    };
 
-  async componentDidMount() {
-    const response = await axios.get("/api/recipes");
-    this.setState({ recipes: response.data });
-  }
+    async componentDidMount() {
+        const response = await axios.get("/api/recipes");
+        this.setState({ recipes: response.data });
+    }
 
-  renderRecipeNames = () => {
-    return this.state.recipes.map(recipe => (
-      <div key={recipe.name} className="ui item">
-        <div className="content">
-          <h4 className="ui header">
-            <Link to={`/recipes/${recipe._id}`}>{recipe.name}</Link>
-          </h4>
-          <div className="description">{recipe.description}</div>
-        </div>
-      </div>
-    ));
-  };
+    renderRecipeNames = () => {
+        return this.state.recipes.map(recipe => (
+            <div key={recipe.name} className="ui item">
+                <div className="content">
+                    <h4 className="ui header">
+                        <Link to={`/recipes/${recipe._id}`}>{recipe.name}</Link>
+                    </h4>
+                    <div className="description">{recipe.description}</div>
+                </div>
+            </div>
+        ));
+    };
 
-  render() {
-    return (
-      <div className="ui padded segment">
-        <h1>Recipes</h1>
-        <div className="ui divided list">{this.renderRecipeNames()}</div>
-        <div className="ui button">
-          <Link to="/create">Create Recipe</Link>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="ui padded segment">
+                <h1>Recipes</h1>
+                <div className="ui divided list">
+                    {this.renderRecipeNames()}
+                </div>
+                <div className="ui button">
+                    <Link to="/create">Create Recipe</Link>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default RecipeList;
